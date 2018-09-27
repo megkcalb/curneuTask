c23173743a6afd4050ccfa85ffad3d1b3f0607a6
Meganathan M <37172226+megkcalb@users.noreply.github.com>
2018-09-28 00:23:29 +0530

    Update camOpen.py
    Identifying the pen which is trained from the image ( already trained ) with the rectangular box.

--- camOpen.py
+++ camOpen.py
@@ -1,22 +1,47 @@
 import cv2
-cam = cv2.VideoCapture(0)
-cv2.namedWindow("test")
-img_counter = 0
+from matplotlib import pyplot as plt
+MIN_MATCH_COUNT=30
+detector = cv2.xfeatures2d_SIFT
+
+cv2.__version__
+import numpy as np
+#detector=cv2.SIFT()
+
+FLANN_INDEX_KDITREE=0
+flannParam=dict(algorithm=FLANN_INDEX_KDITREE,tree=5)
+flann=cv2.FlannBasedMatcher(flannParam,{})
+   #Rendering the image
+trainImg=cv2.imread("E:\placement\curneu\penType1.jpg",0)
+(trainK,trainDes) =detector.detectAndCompute(trainImg,None)
+#detecting from the webcam
+cam=cv2.VideoCapture(0)
 while True:
-    ret, frame = cam.read()
-    cv2.imshow("test", frame)
-    if not ret:
+    ret, QueryImgBGR=cam.read()
+    QueryImg=cv2.cvtColor(QueryImgBGR,cv2.COLOR_BGR2GRAY)
+    queryKP,queryDesc=detector.detectAndCompute(QueryImg,None)
+    matches=flann.knnMatch(queryDesc,trainDes,k=2)   #Matching with the real live objects
+
+    goodMatch=[]
+    for m,n in matches:
+        if(m.distance<0.75*n.distance):
+            goodMatch.append(m)       #appending with the matching parameters
+    if(len(goodMatch)>MIN_MATCH_COUNT):
+        tp=[]
+        qp=[]
+        for m in goodMatch:
+            tp.append(trainK[m.trainIdx].pt)
+            qp.append(queryKP[m.queryIdx].pt)
+        tp,qp=np.float32((tp,qp))
+        H,status=cv2.findHomography(tp,qp,cv2.RANSAC,3.0)
+        h,w=trainImg.shape
+        trainBorder=np.float32([[[0,0],[0,h-1],[w-1,h-1],[w-1,0]]])
+        queryBorder=cv2.perspectiveTransform(trainBorder,H)
+        #Covering over the rectangle box while it detects
+        cv2.polylines(QueryImgBGR,[np.int32(queryBorder)],True,(0,255,0),5)
+    else:
+        print "Not Enough match found- %d/%d"%(len(goodMatch),MIN_MATCH_COUNT)
+    cv2.imshow('result',QueryImgBGR)
+    if cv2.waitKey(10)==ord('q'):
         break
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-         #Receiving escape to intimate closing webcam
-        print("Escape entered .. Closing webcam..")
-        break
-    elif k%256 == 32:
-        #Receiving space to intimate observe
-        img_name = "observed_image{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} uploaded!".format(img_name))
-        img_counter += 1
 cam.release()
 cv2.destroyAllWindows()
